ad22ca2a4f1001993738f7e8f241a3b84dacf861
Mohammadali <63012627+mohammadali-davarzani@users.noreply.github.com>
2021-06-04 13:04:15 +0430

    Update admin.py

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
-from . import models
+from .models import Post, Comment
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_date', 'status',)
     list_filter = ('title', 'author', 'status',)
     search_fields = ('title',)
 
-
+    
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_date', 'active',)
     list_filter = ('name', 'email', 'active',)
     search_fields = ('comment',)
 
-
-admin.site.register(models.Post, PostAdmin)
-admin.site.register(models.Comment, CommentAdmin)
